925454053fb0bc60d0f0bca5fd9b28e24e4229e3
Ryou <106043886+RyouuSama@users.noreply.github.com>
2023-04-24 08:48:18 -0700

    error handling add-on
    error handling was added to the indexHandler function to ensure that the web server works correctly even in unexpected situations. If any error occurs when loading the HTML template, an HTTP 500 error (Internal Server Error) is returned and an error message is displayed to the user.
    
    In addition, the log.Fatal function is used to handle any errors that occur when starting the web server. This ensures that the program will stop in case of a critical error instead of continuing to run with undefined behaviour.

--- main.go
+++ main.go
@@ -2,15 +2,25 @@ package main
 
 import (
 	"html/template"
+	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", indexHandler)
-	http.ListenAndServe(":8080", nil)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("templates/index.html")
-	t.Execute(w, nil)
+	t, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = t.Execute(w, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
